14e4a19696500bfae259fa71631863346d4d779f
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-07-03 22:08:14 -0700

    Add files via upload

---
+++ src/global.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <base/log.h>
+#include <Actor/Actor.h>
+#include <Actor/Player.h>
+#include <mods/CommandSupport.h>
+#include <Item/ItemStack.h>
+#include <Item/Item.h>
+
+#include "Enchant.h"
+#include "EnchantResult.h"
+
+DEF_LOGGER("GiveNbt");
+
+void initCommand(CommandRegistry *registry);
